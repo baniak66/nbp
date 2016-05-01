1b000146174b57a2b83184f23c05f2a408b47665
Pawel Licznar <pawellicznar@gmail.com>
2016-05-01 14:53:56 +0200

    Add routes for money controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Tshtask::Application.routes.draw do
-  root :to => "home#index"
+  root :to => "money#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users, except: [:show, :index]
   resources :money, except: [:delete, :edit, :update, :create, :new]
+  get '/report', to: 'money#report', as: 'report'
+  get '/refresh', to: 'money#refresh_rates', as: 'refresh'
 end
