58b7ce59c2702a7ee2d9f1760f855122e718a5a3
Pawel Licznar <pawellicznar@gmail.com>
2016-05-01 14:58:45 +0200

    Create templates for index, show, report actions

--- app/controllers/money_controller.rb
+++ app/controllers/money_controller.rb
@@ -3,16 +3,18 @@ class MoneyController < ApplicationController
   def index
     #show list of exchange rates with creation time
     #don't forget about pagination
+    @exchanges = Exchange.all
   end
 
   def show
     #show table of currencies for selected exchange rate
+    @exchange = Exchange.find(params[:id])
   end
 
   def refresh_rates
     #for manual refreshing
     #get latest exchange rates and save to db
-    #can be helpful: 
+    #can be helpful:
     #http://www.nbp.pl/home.aspx?f=/kursy/instrukcja_pobierania_kursow_walut.html
   end
 
@@ -21,7 +23,7 @@ class MoneyController < ApplicationController
     #report should show: basic statistics: mean, median, average
     #also You can generate a simple chart(use can use some js library)
 
-    #this method should be available only for currencies which exist in the database 
+    #this method should be available only for currencies which exist in the database
   end
 
 
